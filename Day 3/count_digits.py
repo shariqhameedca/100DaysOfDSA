3451fcc53508af1d94a95b881ab5300a9138b8cd
ShariqHameed <57900267+shariqhameedca@users.noreply.github.com>
2023-07-25 08:36:33 -0700

    Delete Day 3 directory

--- Day 3/count_digits.py
+++
@@ -1,9 +0,0 @@
-number = 1
-
-count = 0
-while number > 0:
-    count += 1
-    number /= 10
-    number = int(number)
-
-print(count)
--- Day 3/extract_digits.py
+++
@@ -1,9 +0,0 @@
-number = 7789
-digits = []
-
-while number > 0:
-    digits.append(number % 10)
-    number /= 10
-    number = int(number)
-
-print(digits)
